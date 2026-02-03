5051a60257b47fb6e9048b7ef8eb7fe4f9c7c21e
htetsoemoe <soemoehtet.mdy@gmail.com>
2026-02-03 22:11:29 +0630

    implementing create a file command

--- 4.SectionFour - Understanding File System/File System Project/app.js
+++ 4.SectionFour - Understanding File System/File System Project/app.js
@@ -1,7 +1,30 @@
 const fs = require("fs/promises");
 
 (async () => {
-    const commandFileHandler = await fs.open("./command.txt", "r");
+
+    // Implementing Create a file command
+    // Command: create a file <path>
+    const CREATE_FILE = "create a file";
+
+    // Create a file handler 
+    const createFileHandler = async (path) => {
+        try {
+            // We want to check whether or not we already have that file
+            const existingFileHandle = await fs.open(path, "r");
+            existingFileHandle.close();
+
+            // We already have that file
+            return console.log(`The File ${path} already exists`);
+        } catch (error) {
+            // We don't have that file, so we can create it
+            const newFileHandle = await fs.open(path, "w"); // 'w' means open file for writing. The file is created (if it does not exist) or truncated (if it exists)
+            newFileHandle.close();
+            return console.log(`The File ${path} has been created successfully`);
+        }
+    }
+
+    // Open the command.txt file
+    const commandFileHandler = await fs.open("./command.txt", "r"); // 'r' means open file for reading. An exception occurs if the file does not exists
 
     commandFileHandler.on("change", async () => {
         // Get the size of our file
@@ -18,9 +41,18 @@ const fs = require("fs/promises");
 
         // We always want to read the whole content (from beginning to end): stored in buff
         await commandFileHandler.read(buff, offset, length, position);
-        console.log(buff.toString("utf-8"));
+        const command = buff.toString("utf-8");
+
+        // Create a file command:
+        // create a file <path>
+        if (command.includes(CREATE_FILE)) {
+            const filePath = command.substring(CREATE_FILE.length + 1);
+            createFileHandler(filePath);
+        }
     })
 
+    // Watcher to monitor changes in command.txt
+    // Returns an async iterator that watches for changes on filename, where filename is either a file or a directory.
     const watcher = fs.watch("./command.txt");
     for await (const event of watcher) {
         if (event.eventType === "change") {
