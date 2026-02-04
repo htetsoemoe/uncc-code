dcb9651ab25ec4730fe0bec2387281a8ff936d3e
htetsoemoe <soemoehtet.mdy@gmail.com>
2026-02-04 20:57:09 +0630

    deleteAFile renameAFile addToAFile template

--- 4.SectionFour - Understanding File System/File System Project/app.js
+++ 4.SectionFour - Understanding File System/File System Project/app.js
@@ -6,6 +6,15 @@ const fs = require("fs/promises");
     // Command: create a file <path>
     const CREATE_FILE = "create a file";
 
+    // Command: delete a file <>
+    const DELETE_FILE = "delete a file";
+
+    // Command: rename a file <oldPath> to <newPath>
+    const RENAME_FILE = "rename a file";
+
+    // Command: add to a file <path> content <content>
+    const ADD_TO_FILE = "add to a file";
+
     // Create a file handler 
     const createFileHandler = async (path) => {
         try {
@@ -21,6 +30,22 @@ const fs = require("fs/promises");
             newFileHandle.close();
             return console.log(`The File ${path} has been created successfully`);
         }
+    }
+
+    // Delete a file handler
+    const deleteFileHandler = async (path) => {
+        console.log(`Deleting ${path}...`)
+    }
+
+    // Rename a file handler
+    const renameFileHandler = async (oldPath, newPath) => {
+        console.log(`Renaming ${oldPath} to ${newPath}`)
+    }
+
+    // Add to a file handler
+    const addToFileHandler = async (path, content) => {
+        console.log(`Adding to ${path}... `)
+        console.log(`the content: ${content}...`)
     }
 
     // Open the command.txt file
@@ -48,6 +73,31 @@ const fs = require("fs/promises");
         if (command.includes(CREATE_FILE)) {
             const filePath = command.substring(CREATE_FILE.length + 1);
             createFileHandler(filePath);
+        }
+
+        // Delete a file command:
+        // delete a file <path>
+        if (command.includes(DELETE_FILE)) {
+            const filePath = command.substring(DELETE_FILE.length + 1);
+            deleteFileHandler(filePath);
+        }
+
+        // Rename a file command:
+        // rename a file <oldPath> to <newPath>
+        if (command.includes(RENAME_FILE)) {
+            const _idx = command.indexOf(" to ");
+            const oldPath = command.substring(RENAME_FILE.length + 1, _idx);
+            const newPath = command.substring(_idx + 4);
+            renameFileHandler(oldPath, newPath);
+        }
+
+        // Add to a file command:
+        // add to a file <path> this content: <content>
+        if (command.includes(ADD_TO_FILE)) {
+            const _idx = command.indexOf(" this content: ");
+            const filePath = command.substring(ADD_TO_FILE.length + 1, _idx);
+            const content = command.substring(_idx + 15);
+            addToFileHandler(filePath, content);
         }
     })
 
