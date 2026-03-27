e3e34f618279895507870ef878e408832169f025
htetsoemoe <soemoehtet.mdy@gmail.com>
2026-03-27 11:57:49 +0630

    simple file upload route

--- 7.SectionRealSeven - Understanding HTTP/4-create-http-server/server.js
+++ 7.SectionRealSeven - Understanding HTTP/4-create-http-server/server.js
@@ -12,6 +12,59 @@ server.on("request", async (req, res) => {
 
         fileStream.pipe(res)
     }
+
+    if (req.url == '/styles.css' && req.method == 'GET') {
+        res.setHeader("Content-Type", "text/css")
+
+        const fileHandle = await fs.open("./public/styles.css", "r")
+        const fileStream = fileHandle.createReadStream
+
+        fileStream.pipe(res)
+    }
+
+    if (req.url === '/scripts.js' && req.method === 'GET') {
+        res.setHeader("Content-Type", "text/javascript")
+
+        const fileHandle = await fs.open("./public/scripts.js", "r")
+        const fileStream = fileHandle.createReadStream()
+
+        fileStream.pipe(res)
+    }
+
+    if (req.url === "/login" && req.method === "POST") {
+        response.setHeader("Content-Type", "application/json");
+        response.statusCode = 200;
+
+        const body = {
+            message: "Logging you in...",
+        };
+
+        response.end(JSON.stringify(body));
+    }
+
+    if (req.url === "/user" && req.method === "PUT") {
+        response.setHeader("Content-Type", "application/json");
+        response.statusCode = 401;
+
+        const body = {
+            message: "You first have to login.",
+        };
+
+        response.end(JSON.stringify(body));
+    }
+
+    // upload route
+    if (req.url === '/upload' && req.method === 'PUT') {
+        const fileHandle = await fs.open("./storage/image.jpeg", "w")
+        const fileStream = fileHandle.createWriteStream()
+        res.setHeader("Content-Type", "application/json")
+
+        req.pipe(fileStream)
+
+        req.on("end", () => {
+            res.end(JSON.stringify({message: "File was uploaded successfully!"}))
+        })
+    }
 })
 
 server.listen(3000, () => {
