2217f260fcee53ba8b1fdd48fae9a76c17f1e44b
htetsoemoe <soemoehtet.mdy@gmail.com>
2026-04-14 15:29:18 +0630

    setup and run web server using butter framework

---
+++ 7.SectionRealSeven - Understanding HTTP/5.1-project-with-mini-framework/poster/server.js
@@ -0,0 +1,51 @@
+const Butter = require("../../butter")
+
+const USERS = [
+    { id: 1, name: "Ko Htet", username: "kohtet", password: "string" },
+    { id: 2, name: "Khin", username: "khin", password: "string" },
+    { id: 3, name: "Ben", username: "ben", password: "string" },
+];
+
+const POSTS = [
+    {
+        id: 1,
+        title: "Node.js",
+        body: "Node.js® is a free, open-source, cross-platform JavaScript runtime environment that lets developers create servers, web apps, command line tools and scripts.",
+        userId: 1,
+    },
+];
+
+const PORT = 8000;
+
+const server = new Butter();
+
+// ------ Files Routes ------ //
+
+server.route("get", "/", (req, res) => {
+    res.sendFile("./public/index.html", "text/html");
+});
+
+server.route("get", "/styles.css", (req, res) => {
+    res.sendFile("./public/styles.css", "text/css");
+});
+
+server.route("get", "/scripts.js", (req, res) => {
+    res.sendFile("./public/scripts.js", "text/javascript");
+});
+
+// ------ JSON Routes ------ //
+
+// Send the list of all the posts that we have
+server.route("get", "/api/posts", (req, res) => {
+    const posts = POSTS.map((post) => {
+        const user = USERS.find((user) => user.id === post.userId);
+        post.author = user.name;
+        return post;
+    });
+
+    res.status(200).json(posts);
+});
+
+server.listen(PORT, () => {
+    console.log(`Server has started on port ${PORT}`);
+});
