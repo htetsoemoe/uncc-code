28ffe2effc348393d6626f511d55f97eda1b5db5
htetsoemoe <soemoehtet.mdy@gmail.com>
2026-04-19 11:12:16 +0630

    unix shell intro

---
+++ 8.SectionEight - Understanding Unix/2-understanding-shell/playground.js
@@ -0,0 +1,21 @@
+const { spawn, exec } = require("node:child_process");
+
+// echo "something string" | tr ' ' '\n'
+
+const subprocess = spawn("bash", ["./scripts.sh"]);
+
+subprocess.stdout.on("data", (data) => {
+    console.log(data.toString("utf-8"));
+});
+
+// tr is 'transform' from ' ' to '\n'
+// exec("echo 'something string' | tr ' ' '\n'", (error, stdout, stderr) => {
+//     if (error) {
+//         console.error(error);
+//         return;
+//     }
+
+//     console.log(stdout);
+
+//     console.log(`stderr: ${stderr}`);
+// });
---
+++ 8.SectionEight - Understanding Unix/2-understanding-shell/scripts.sh
@@ -0,0 +1,6 @@
+x=100
+y=20
+sleep 4
+echo $(($x+$y))
+echo 'something string' | tr ' ' '\n'
+sleep 5
