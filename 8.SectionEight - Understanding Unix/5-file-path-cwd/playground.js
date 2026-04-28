8f32ce99818eed0d5a1b3440249d8ba4df12946d
htetsoemoe <soemoehtet.mdy@gmail.com>
2026-04-28 09:58:46 +0630

    unix file system path pwd

---
+++ 8.SectionEight - Understanding Unix/5-file-path-cwd/playground.js
@@ -0,0 +1,42 @@
+const { spawn, exec } = require("node:child_process");
+
+// echo "something string" | tr ' ' '\n'
+
+// console.log(process.env.PATH);
+// console.log(process.argv);
+// console.log(process.pid);
+// console.log(process.ppid);
+
+console.log(process.env);
+console.log(process.env.MODE);
+console.log(process.env.PATH);
+console.log(process.env.PWD);
+
+const subprocess = spawn(
+  "./playground",
+  ["some string", "-f", 34, "some more string", "-u"],
+  {
+    env: { MODE: "development" },
+  }
+);
+
+subprocess.stdout.on("data", (data) => {
+  console.log(data.toString("utf-8"));
+});
+
+// exec(
+//   " source .bashrc && echo 'something string' | tr ' ' '\n'",
+//   {
+//     shell: "/bin/zsh",
+//   },
+//   (error, stdout, stderr) => {
+//     if (error) {
+//       console.error(error);
+//       return;
+//     }
+
+//     console.log(stdout);
+
+//     console.log(`stderr: ${stderr}`);
+//   }
+// );
