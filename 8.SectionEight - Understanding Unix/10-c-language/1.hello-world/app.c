a79c0ad60d10d9841901ee034a3067c44cb99224
htetsoemoe <soemoehtet.mdy@gmail.com>
2026-05-01 15:07:59 +0630

    start c language

---
+++ 8.SectionEight - Understanding Unix/10-c-language/1.hello-world/app.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    int a = 50;
+    int b = 20;
+    int c = a + b;
+
+    fprintf(stdout, "Hello World from C Programming Language!\n");
+    fprintf(stdout, "This is some string\n");
+
+    return 0;
+}
